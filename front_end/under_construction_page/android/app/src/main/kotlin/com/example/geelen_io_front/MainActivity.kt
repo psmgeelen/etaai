abffc305bd2d41d5a0e5cf272afd11a6db4c3501
Celine Geelen <mlrgeelen@gmail.com>
2023-07-05 17:13:05 +0200

    Add under construction page

---
+++ front_end/under_construction_page/android/app/src/main/kotlin/com/example/geelen_io_front/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geelen_io_front
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ front_end/under_construction_page/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('https://github.com/psmgeelen/etaai');
+
+void main() {
+  runApp(const MyApp());
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw Exception('Could not launch $_url');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.robotoMonoTextTheme(),
+      ),
+      home: Scaffold(
+        backgroundColor: const Color.fromRGBO(49, 49, 58, 1),
+        body: Center(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/logo.jpeg',
+                alignment: Alignment.topCenter,
+                height: 300,
+              ),
+              const SizedBox(
+                height: 80,
+              ),
+              SizedBox(
+                width: 600,
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: const TextStyle(
+                      color: Color(0xfff0f0f0),
+                      fontSize: 22,
+                    ),
+                    children: [
+                      const TextSpan(
+                          style: TextStyle(height: 1.5),
+                          text: 'Welcome to the landing page for η.ai. '
+                              'The project is still under development '
+                              'and will take some time to complete. '
+                              'If you want to follow the development, please visit the '),
+                      const WidgetSpan(
+                        child: FaIcon(
+                          FontAwesomeIcons.github,
+                          color: Color(0xfff0f0f0),
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' repo',
+                        style: const TextStyle(
+                          color: Color(0xfff16c40),
+                        ),
+                        recognizer: TapGestureRecognizer()..onTap = _launchUrl,
+                      ),
+                      const TextSpan(text: '!'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
